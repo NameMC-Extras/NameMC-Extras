8649f2d5726155f3c7ee21b232f9fe2454ef5582
Faav <faavemc@gmail.com>
2022-09-14 15:01:34 -0400

    Update namemc-inject.js

--- js/namemc-inject.js
+++ js/namemc-inject.js
@@ -4,7 +4,6 @@ function endsWithNumber(str) {
 }
 
 if (endsWithNumber(location.pathname)) {
-
   const waitForUUID = function (callback) {
     if (document.querySelector('.order-lg-2')) {
       callback();
@@ -32,6 +31,7 @@ if (endsWithNumber(location.pathname)) {
   });
 
   waitForUUID(async () => {
+    var username = document.querySelector('.text-nowrap[translate=no]').innerText;
     var uuid = document.querySelector('.order-lg-2').innerText;
     var views = document.querySelector('.card-body > :nth-child(3)');
 
@@ -43,6 +43,11 @@ if (endsWithNumber(location.pathname)) {
   <div class="row no-gutters">
     <div class="col col-lg-4"><strong>Creation Date</strong></div>
     <div id="cdate" class="col-auto">Loading... <i class="fal fa-spinner icon-spin"></i></div>
+  </div>
+  <div class="row no-gutters">
+    <div class="col order-lg-1 col-lg-4"><strong>mcuserna.me</strong></div>
+    <div class="col-auto order-lg-3 col-lg-auto text-nowrap text-right"><a class="copy-button" href="javascript:void(0)" data-clipboard-text="mcuserna.me/${uuid}" onclick="return false">Copy</a></div>
+    <div class="col-12 order-lg-2 col-lg"><a href="https://mcuserna.me/${uuid}" rel="nofollow">mcuserna.me/${username}</a></div>
   </div>
   `
 
