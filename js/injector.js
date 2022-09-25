04be7d4663c5755f1b6587b6decda65a0b759593
Faav <faavemc@gmail.com>
2022-09-25 14:33:16 -0400

    Update injector.js

--- js/injector.js
+++ js/injector.js
@@ -1,9 +1,6 @@
 /* copyright 2022 | Faav#6320 | github.com/bribes */
 var inject1 = document.createElement('script');
 inject1.src = chrome.runtime.getURL('js/skinview3d.bundle.js');
-inject1.onload = function() {
-    this.remove();
-};
 (document.head || document.documentElement).appendChild(inject1);
 
 var inject2 = document.createElement('script');
