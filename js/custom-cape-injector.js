776eb3e9f189fa3cfcee1a93a17b84247e78d405
Vafas <faavemc@gmail.com>
2024-01-14 13:55:45 -0500

    Delete js directory

--- js/custom-cape-injector.js
+++
@@ -1,7 +0,0 @@
-/* copyright 2024 | Faav#6320 | github.com/bribes */
-var inject = document.createElement('script');
-inject.src = chrome.runtime.getURL('js/skinview3d.bundle.js');
-inject.onload = function() {
-    this.remove();
-};
-(document.head || document.documentElement).appendChild(inject);
