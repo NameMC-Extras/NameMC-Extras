5a1444dfeedf20a60473e9994fed13814d2b1ecb
Vafas <faavemc@gmail.com>
2024-01-17 22:05:14 -0500

    Move to supabase

--- js/supabase/supabase-manager.js
+++ js/supabase/supabase-manager.js
@@ -1,12 +1,17 @@
 let client = null;
 
+async function injectFunc(injectedFunction) {
+  var inject = document.createElement('iframe');
+  inject.srcdoc = `<script>${"window.top." + injectedFunction.name + "=" + JSON.stringify(await injectedFunction())}</script>`;
+
+  (document.head || document.documentElement).appendChild(inject);
+}
+
 function getSupabaseClient() {
   if (client) return client;
   client = window.supabase.createClient('https://cesolgpkhjyzprrejqzn.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNlc29sZ3BraGp5enBycmVqcXpuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDU0NTMxMTcsImV4cCI6MjAyMTAyOTExN30.kQO7yzcNRTHUIjC-xIQPVNK2HPSqWl_kh1PrZHvdifI');
   return client;
 }
-
-
 
 class SupabaseCapeOwnership {
   constructor(uuid, note) {
@@ -70,38 +75,6 @@ class SupabaseCapeCategory {
   }
 }
 
-
-
-/**
- * Returns an array of cape categories
- * @returns {Promise<SupabaseCapeCategory[]>}
- */
-async function getAllCategories() {
-  const { data, error } = await getSupabaseClient()
-    .from('cape_categories')
-    .select();
-  if (error) {
-    console.warn(error.message);
-    return [];
-  }
-  return data.map(v => new SupabaseCapeCategory(v.id, v.name, v.hidden));
-}
-
-/**
- * Returns an array of capes
- * @returns {Promise<SupabaseCape[]>}
- */
-async function getAllCapes() {
-  const { data, error } = await getSupabaseClient()
-    .from('capes')
-    .select();
-  if (error) {
-    console.warn(error.message);
-    return [];
-  }
-  return data.map(v => new SupabaseCape(v.id, v.name, v.description, v.image_src, v.image_render, v.category));
-}
-
 /**
  * Returns a cape category or null if non-existent
  * @param {string} id 
@@ -118,6 +91,21 @@ async function getCapeCategory(id) {
     return null;
   }
   return data ? new SupabaseCapeCategory(data.id, data.name) : null;
+}
+async function getUsers(id) {
+  const { data, error } = await getSupabaseClient()
+    .from('user_capes')
+    .select()
+    .eq('cape', id);
+  if (error) {
+    console.warn(error.message);
+    return [];
+  }
+  return data.map(v => new SupabaseCapeOwnership(v.user, v.note));
+}
+
+function getURL(category, id) {
+  return `https://namemc.com/cape/${category}/${id.replace("-", "")}`
 }
 
 /**
