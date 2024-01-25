02b240c9e2e6d506f06e39b3cf45047823e92114
Vafas <faavemc@gmail.com>
2024-01-24 22:38:42 -0500

    efficiency

--- pages/skin-cape-test.js
+++ pages/skin-cape-test.js
@@ -1,13 +1,15 @@
-var paused = false;
+// only use for getting animate cookie
+function getCookie(name) {
+    let cookies = Object.fromEntries(document.cookie.split(';').map(e => e.split('=').map(e => decodeURIComponent(e.trim()))));
+    return cookies[name];
+}
+
+var paused = (getCookie("animate") === "false");
 var elytraOn = false;
 var layer = true;
 var currentCape = null;
 var currentOptifineMode = "steal"
 var specialCapes = {};
-
-var jsColor = document.createElement("script");
-jsColor.src = 'https://cdnjs.cloudflare.com/ajax/libs/jscolor/2.4.5/jscolor.min.js';
-document.head.appendChild(jsColor)
 
 const waitForSelector = function (selector, callback) {
     query = document.querySelector(selector)
@@ -30,7 +32,15 @@ const waitForFunc = function (func, callback) {
     }
 };
 
-
+const waitForSupabase = function (callback) {
+    if (window.localStorage.getItem("supabase_data") && window.localStorage.getItem("supabase_data").length != 0) {
+        callback(JSON.parse(window.localStorage.getItem("supabase_data")));
+    } else {
+        setTimeout(function () {
+            waitForSupabase(callback);
+        });
+    }
+};
 
 // toggle skin layers
 const toggleLayers = () => {
@@ -75,6 +85,7 @@ const fixPauseBtn = () => {
                 pauseIcon.classList.remove('fa-play');
                 pauseIcon.classList.add('fa-pause');
             }
+            setCookie("animate", !paused);
             skinViewer.animation.paused = paused;
         }
     })
@@ -138,7 +149,7 @@ const createElytraBtn = () => {
 }
 
 // get/cache special capes
-async function getSpecialCapes() {
+function getSpecialCapes(supabase_data) {
     if (Object.keys(specialCapes).length > 0) return specialCapes;
     supabase_data.categories.forEach(cat => {
         specialCapes[cat.name] = supabase_data.capes.filter(cape => cape.category == cat.id);
@@ -146,10 +157,10 @@ async function getSpecialCapes() {
     return specialCapes;
 }
 
+waitForSupabase(supabase_data => {
+    waitForSelector('main', (main) => {
+        main.style["margin-top"] = "1rem";
 
-waitForFunc("supabase_data", async () => {
-    getSpecialCapes()
-    waitForSelector('main', async (main) => {
         main.innerHTML = `<h1 class="text-center">Skin & Cape Tester</h1>
         <hr class="mt-0">
         <div class="row">
@@ -238,7 +249,7 @@ waitForFunc("supabase_data", async () => {
 
             skinViewer.animation = new skinview3d.WalkingAnimation();
             skinViewer.animation.speed = 0.5;
-            skinViewer.animation.paused = false;
+            skinViewer.animation.paused = paused;
             skinViewer.animation.headBobbing = false;
 
             window.skinViewer = skinViewer;
@@ -318,71 +329,71 @@ waitForFunc("supabase_data", async () => {
                 if (vanilla.checked == true) {
                     capemenu.innerHTML = `<label class="col-4 col-form-label" for="officialcapes"><strong>Official Capes:</strong></label>
                 <div class="col">
-                    <select class="form-select" id="officialcapes" name="officialcapes">
+                    <select class="form-select" id="officialcapes" name="officialcapes" required>
                         <optgroup label="Common">
-                            <option data-value="2340c0e03dd24a11b15a8b33c2a7e9e32abb2051b2481d0ba7defd635ca7a933">Migrator</option>
-                            <option data-value="f9a76537647989f9a0b6d001e320dac591c359e9e61a31f4ce11c88f207f0ad4">Vanilla</option>
-                            <option data-value="afd553b39358a24edfe3b8a9a939fa5fa4faa4d9a9c3d6af8eafb377fa05c2bb">Cherry Blossom
+                            <option value="2340c0e03dd24a11b15a8b33c2a7e9e32abb2051b2481d0ba7defd635ca7a933">Migrator</option>
+                            <option value="f9a76537647989f9a0b6d001e320dac591c359e9e61a31f4ce11c88f207f0ad4">Vanilla</option>
+                            <option value="afd553b39358a24edfe3b8a9a939fa5fa4faa4d9a9c3d6af8eafb377fa05c2bb">Cherry Blossom
                             </option>
                         </optgroup>
                         <optgroup label="Minecon">
-                            <option data-value="e7dfea16dc83c97df01a12fabbd1216359c0cd0ea42f9999b6e97c584963e980">Minecon 2016</option>
-                            <option data-value="b0cc08840700447322d953a02b965f1d65a13a603bf64b17c803c21446fe1635">Minecon 2015</option>
-                            <option data-value="153b1a0dfcbae953cdeb6f2c2bf6bf79943239b1372780da44bcbb29273131da">Minecon 2013</option>
-                            <option data-value="a2e8d97ec79100e90a75d369d1b3ba81273c4f82bc1b737e934eed4a854be1b6">Minecon 2012</option>
-                            <option data-value="953cac8b779fe41383e675ee2b86071a71658f2180f56fbce8aa315ea70e2ed6">Minecon 2011</option>
+                            <option value="e7dfea16dc83c97df01a12fabbd1216359c0cd0ea42f9999b6e97c584963e980">Minecon 2016</option>
+                            <option value="b0cc08840700447322d953a02b965f1d65a13a603bf64b17c803c21446fe1635">Minecon 2015</option>
+                            <option value="153b1a0dfcbae953cdeb6f2c2bf6bf79943239b1372780da44bcbb29273131da">Minecon 2013</option>
+                            <option value="a2e8d97ec79100e90a75d369d1b3ba81273c4f82bc1b737e934eed4a854be1b6">Minecon 2012</option>
+                            <option value="953cac8b779fe41383e675ee2b86071a71658f2180f56fbce8aa315ea70e2ed6">Minecon 2011</option>
                         </optgroup>
                         <optgroup label="Special">
-                            <option data-value="17912790ff164b93196f08ba71d0e62129304776d0f347334f8a6eae509f8a56">Realms Mapmaker
+                            <option value="17912790ff164b93196f08ba71d0e62129304776d0f347334f8a6eae509f8a56">Realms Mapmaker
                             </option>
-                            <option data-value="9e507afc56359978a3eb3e32367042b853cddd0995d17d0da995662913fb00f7">Mojang (New)</option>
-                            <option data-value="5786fe99be377dfb6858859f926c4dbc995751e91cee373468c5fbf4865e7151">Mojang</option>
-                            <option data-value="8f120319222a9f4a104e2f5cb97b2cda93199a2ee9e1585cb8d09d6f687cb761">Mojang (Classic)
+                            <option value="9e507afc56359978a3eb3e32367042b853cddd0995d17d0da995662913fb00f7">Mojang (New)</option>
+                            <option value="5786fe99be377dfb6858859f926c4dbc995751e91cee373468c5fbf4865e7151">Mojang</option>
+                            <option value="8f120319222a9f4a104e2f5cb97b2cda93199a2ee9e1585cb8d09d6f687cb761">Mojang (Classic)
                             </option>
-                            <option data-value="ae677f7d98ac70a533713518416df4452fe5700365c09cf45d0d156ea9396551">Mojira Moderator
+                            <option value="ae677f7d98ac70a533713518416df4452fe5700365c09cf45d0d156ea9396551">Mojira Moderator
                             </option>
-                            <option data-value="1bf91499701404e21bd46b0191d63239a4ef76ebde88d27e4d430ac211df681e">Translator</option>
-                            <option data-value="2262fb1d24912209490586ecae98aca8500df3eff91f2a07da37ee524e7e3cb6">Translator (Chinese)
+                            <option value="1bf91499701404e21bd46b0191d63239a4ef76ebde88d27e4d430ac211df681e">Translator</option>
+                            <option value="2262fb1d24912209490586ecae98aca8500df3eff91f2a07da37ee524e7e3cb6">Translator (Chinese)
                             </option>
-                            <option data-value="ca35c56efe71ed290385f4ab5346a1826b546a54d519e6a3ff01efa01acce81">Cobalt</option>
-                            <option data-value="3efadf6510961830f9fcc077f19b4daf286d502b5f5aafbd807c7bbffcaca245">Scrolls</option>
+                            <option value="ca35c56efe71ed290385f4ab5346a1826b546a54d519e6a3ff01efa01acce81">Cobalt</option>
+                            <option value="3efadf6510961830f9fcc077f19b4daf286d502b5f5aafbd807c7bbffcaca245">Scrolls</option>
                         </optgroup>
                         <optgroup label="Custom">
-                            <option data-value="5048ea61566353397247d2b7d946034de926b997d5e66c86483dfb1e031aee95">Turtle</option>
-                            <option data-value="2056f2eebd759cce93460907186ef44e9192954ae12b227d817eb4b55627a7fc">Birthday</option>
-                            <option data-value="6a7cf0eb5cfe7e7c508b364e32916dfd28d164e7bf6d92c6ea7811b82451e760">Valentine</option>
-                            <option data-value="bcfbe84c6542a4a5c213c1cacf8979b5e913dcb4ad783a8b80e3c4a7d5c8bdac">dannyBstyle</option>
-                            <option data-value="70efffaf86fe5bc089608d3cb297d3e276b9eb7a8f9f2fe6659c23a2d8b18edf">Millionth Customer
+                            <option value="5048ea61566353397247d2b7d946034de926b997d5e66c86483dfb1e031aee95">Turtle</option>
+                            <option value="2056f2eebd759cce93460907186ef44e9192954ae12b227d817eb4b55627a7fc">Birthday</option>
+                            <option value="6a7cf0eb5cfe7e7c508b364e32916dfd28d164e7bf6d92c6ea7811b82451e760">Valentine</option>
+                            <option value="bcfbe84c6542a4a5c213c1cacf8979b5e913dcb4ad783a8b80e3c4a7d5c8bdac">dannyBstyle</option>
+                            <option value="70efffaf86fe5bc089608d3cb297d3e276b9eb7a8f9f2fe6659c23a2d8b18edf">Millionth Customer
                             </option>
-                            <option data-value="d8f8d13a1adf9636a16c31d47f3ecc9bb8d8533108aa5ad2a01b13b1a0c55eac">Prismarine</option>
-                            <option data-value="23ec737f18bfe4b547c95935fc297dd767bb84ee55bfd855144d279ac9bfd9fe">JulianClark</option>
-                            <option data-value="2e002d5e1758e79ba51d08d92a0f3a95119f2f435ae7704916507b6c565a7da8">MrMessiah</option>
-                            <option data-value="ca29f5dd9e94fb1748203b92e36b66fda80750c87ebc18d6eafdb0e28cc1d05f">cheapsh0t</option>
+                            <option value="d8f8d13a1adf9636a16c31d47f3ecc9bb8d8533108aa5ad2a01b13b1a0c55eac">Prismarine</option>
+                            <option value="23ec737f18bfe4b547c95935fc297dd767bb84ee55bfd855144d279ac9bfd9fe">JulianClark</option>
+                            <option value="2e002d5e1758e79ba51d08d92a0f3a95119f2f435ae7704916507b6c565a7da8">MrMessiah</option>
+                            <option value="ca29f5dd9e94fb1748203b92e36b66fda80750c87ebc18d6eafdb0e28cc1d05f">cheapsh0t</option>
                         </optgroup>
                         <optgroup label="Previous or Temporary Capes">
-                            <option data-value="fd14214cd8073059e93d9c626260f5df85e5a959181537119df56cadaf5002cc">Bacon</option>
-                            <option data-value="2ada7acf3e0ef436f350e21af91a774b7cd95309c53668a441eeacec88ca4211">Christmas 2010
+                            <option value="fd14214cd8073059e93d9c626260f5df85e5a959181537119df56cadaf5002cc">Bacon</option>
+                            <option value="2ada7acf3e0ef436f350e21af91a774b7cd95309c53668a441eeacec88ca4211">Christmas 2010
                             </option>
-                            <option data-value="d1f20f8534f9f58a3a0a26586d5615f513add564809986334b7f247593425ee3">New Year's 2011
+                            <option value="d1f20f8534f9f58a3a0a26586d5615f513add564809986334b7f247593425ee3">New Year's 2011
                             </option>
-                            <option data-value="938155dd83118a3993a22579649fab313cdb06073029c3839843d58fad06ebb2">Xbox 360 1st Birthday
+                            <option value="938155dd83118a3993a22579649fab313cdb06073029c3839843d58fad06ebb2">Xbox 360 1st Birthday
                                 Cape</option>
                         </optgroup>
                         <optgroup label="Bedrock Capes">
-                            <option data-value="99aba02ef05ec6aa4d42db8ee43796d6cd50e4b2954ab29f0caeb85f96bf52a1">Founder's Cape
+                            <option value="99aba02ef05ec6aa4d42db8ee43796d6cd50e4b2954ab29f0caeb85f96bf52a1">Founder's Cape
                             </option>
-                            <option data-value="28de4a81688ad18b49e735a273e086c18f1e3966956123ccb574034c06f5d336">Pancape</option>
+                            <option value="28de4a81688ad18b49e735a273e086c18f1e3966956123ccb574034c06f5d336">Pancape</option>
                         </optgroup>
                     </select>
                 </div>`;
 
-                    currentCape = "https://textures.minecraft.net/texture/" + $('#officialcapes :selected').data('value');
+                    currentCape = "https://textures.minecraft.net/texture/" + document.querySelector("#officialcapes option:checked").value;
 
                     capemenu.style.display = 'unset';
 
                     officialcapes.onchange = () => {
                         if (officialcapes.value && officialcapes.value.length > 0) {
-                            currentCape = "https://textures.minecraft.net/texture/" + $('#officialcapes :selected').data('value');
+                            currentCape = "https://textures.minecraft.net/texture/" + document.querySelector("#officialcapes option:checked").value;
                         }
                     }
                 }
@@ -513,9 +524,9 @@ waitForFunc("supabase_data", async () => {
                 }
             }
 
-            special.onchange = async () => {
+            special.onchange = () => {
                 if (special.checked) {
-                    const dictionary = await getSpecialCapes();
+                    const dictionary = getSpecialCapes(supabase_data);
                     capemenu.innerHTML = `
                     <label class="col-4 col-form-label" for="specialcapes"><strong>Special Capes:</strong></label>
                     <div class="col">
@@ -523,27 +534,33 @@ waitForFunc("supabase_data", async () => {
                             ${Object.keys(dictionary).map(key => {
                         const categoryName = key
                         const capes = dictionary[key]
-                        return `
-                                    <optgroup label="${categoryName}">
-                                        ${capes.sort((a, b) => a.name.localeCompare(b.name)).map(c => `<option data-value="${c.image_src}">${c.name}</option>`).join("")}
-                                    </optgroup>
-                                `;
+                        var optGroupEl = document.createElement("optgroup");
+                        optGroupEl.label = categoryName;
+                        optGroupEl.innerHTML = capes.sort((a, b) => a.name.localeCompare(b.name)).map(c => {
+                            var optionEl = document.createElement("option");
+                            optionEl.value = c.image_src;
+                            optionEl.textContent = c.name;
+
+                            return optionEl.outerHTML;
+                        }).join("")
+
+                        return optGroupEl.outerHTML;
                     }).join("")}
                         </select>
                     </div>
                 `;
 
-                    currentCape = $('#specialcapes :selected').data('value');
+                    currentCape = document.querySelector("#specialcapes option:checked").value;
 
                     capemenu.style.display = 'unset';
 
                     specialcapes.onchange = () => {
                         if (specialcapes.value && specialcapes.value.length > 0) {
-                            currentCape = $('#specialcapes :selected').data('value');
+                            currentCape = document.querySelector("#specialcapes option:checked").value;
                         }
                     }
                 }
             }
-        })
+        });
     });
 });
