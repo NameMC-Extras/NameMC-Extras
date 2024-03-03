85c617ee50bed99190a9e418c29ad9857f369c6a
Vafas <faavemc@gmail.com>
2024-03-03 17:00:18 -0500

    Fix via await

--- js/page-injector.js
+++ js/page-injector.js
@@ -1,100 +1,103 @@
 /* copyright 2024 | Faav#6320 | github.com/bribes */
-const waitForSelector = function (selector, callback) {
-    query = document.querySelector(selector)
-    if (query) {
-        setTimeout((query) => {
-            callback(query);
-        }, null, query);
-    } else {
-        setTimeout(() => {
-            waitForSelector(selector, callback);
-        });
+(async () => {
+    const waitForSelector = function (selector, callback) {
+        query = document.querySelector(selector)
+        if (query) {
+            setTimeout((query) => {
+                callback(query);
+            }, null, query);
+        } else {
+            setTimeout(() => {
+                waitForSelector(selector, callback);
+            });
+        }
+    };
+
+    const customPage = async (page, name, title, icon) => {
+        waitForSelector('[href="/minecraft-skins"]', () => {
+            var isPage = location.pathname == "/extras/" + page;
+            var storeNavBar = document.querySelector('.nav-link[href="https://store.namemc.com/category/emerald"]').parentElement;
+            var customNavRange = document.createRange();
+            var customNavHTML = customNavRange.createContextualFragment(`<li class='nav-item'><a class='nav-link ${isPage ? "active" : ""}' href='/extras/${page}'>${name}</a></li>`);
+            var customNavDropRange = document.createRange();
+            var customNavDropHTML = customNavDropRange.createContextualFragment(`<a class='dropdown-item' id='${page}' href='/extras/${page}' title='${name}'><i class="${icon} menu-icon"></i>${name}</a>`);
+            storeNavBar.before(customNavHTML);
+            waitForSelector('.dropdown-divider:nth-of-type(2)', (dropDivider) => dropDivider.before(customNavDropHTML));
+
+            if (isPage === true) {
+                document.title = title + " | NameMC Extras";
+
+                var inject1 = document.createElement('script');
+                inject1.src = chrome.runtime.getURL(`pages/${page}.js`);
+                inject1.onload = function () {
+                    this.remove();
+                };
+                (document.head || document.documentElement).appendChild(inject1);
+
+                var inject2 = document.createElement('script');
+                inject2.src = chrome.runtime.getURL('js/skinview3d.bundle.js');
+                inject2.onload = function () {
+                    this.remove();
+                };
+                (document.head || document.documentElement).appendChild(inject2);
+
+                var inject3 = document.createElement('script');
+                inject3.src = chrome.runtime.getURL('js/jscolor.min.js');
+                inject3.onload = function () {
+                    this.remove();
+                };
+                (document.head || document.documentElement).appendChild(inject3);
+
+                waitForSelector('#faq', (faq) => {
+                    faq.remove()
+                });
+
+                document.querySelector('.dropdown-item.active')?.classList.remove('active');
+                document.querySelector('#' + page)?.classList.add('active');
+            }
+        })
     }
-};
 
-const customPage = (page, name, title, icon) => {
-    waitForSelector('[href="/minecraft-skins"]', () => {
-        var isPage = location.pathname == "/extras/" + page;
-        var storeNavBar = document.querySelector('.nav-link[href="https://store.namemc.com/category/emerald"]').parentElement;
-        var customNavRange = document.createRange();
-        var customNavHTML = customNavRange.createContextualFragment(`<li class='nav-item'><a class='nav-link ${isPage ? "active" : ""}' href='/extras/${page}'>${name}</a></li>`);
-        var customNavDropRange = document.createRange();
-        var customNavDropHTML = customNavDropRange.createContextualFragment(`<a class='dropdown-item' id='${page}' href='/extras/${page}' title='${name}'><i class="${icon} menu-icon"></i>${name}</a>`);
-        storeNavBar.before(customNavHTML);
-        waitForSelector('.dropdown-divider:nth-of-type(2)', (dropDivider) => dropDivider.before(customNavDropHTML));
+    const customMenuItem = async (id, name, href, location, classes) => {
+        waitForSelector('[href="/my-account"]', (myAccountBtn) => {
+            var dropDownMenu = myAccountBtn.parentElement;
+            var menuItem = document.createElement("a")
+            menuItem.classList.add("dropdown-item");
+            menuItem.id = id;
+            menuItem.href = href;
+            menuItem.textContent = name;
+            if (classes) {
+                var iconEl = document.createElement("i");
+                iconEl.setAttribute("class", classes + " menu-icon")
 
-        if (isPage === true) {
-            document.title = title + " | NameMC Extras";
+                menuItem.insertBefore(iconEl, menuItem.lastChild);
+            }
+
+            dropDownMenu.insertBefore(menuItem, dropDownMenu.childNodes[location]);
 
             var inject1 = document.createElement('script');
-            inject1.src = chrome.runtime.getURL(`pages/${page}.js`);
+            inject1.src = chrome.runtime.getURL(`dropdown-items/${id}.js`);
             inject1.onload = function () {
                 this.remove();
             };
             (document.head || document.documentElement).appendChild(inject1);
+        })
+    }
 
-            var inject2 = document.createElement('script');
-            inject2.src = chrome.runtime.getURL('js/skinview3d.bundle.js');
-            inject2.onload = function () {
-                this.remove();
-            };
-            (document.head || document.documentElement).appendChild(inject2);
+    // INJECTING PAGES
 
-            var inject3 = document.createElement('script');
-            inject3.src = chrome.runtime.getURL('js/jscolor.min.js');
-            inject3.onload = function () {
-                this.remove();
-            };
-            (document.head || document.documentElement).appendChild(inject3);
+    await customPage('skin-cape-test', 'Tester', 'Skin & Cape Tester', 'fas fa-rectangle-portrait')
+    await customPage('badges', 'Badges', 'Badges', 'fas fa-award')
 
-            waitForSelector('#faq', (faq) => {
-                faq.remove()
-            });
+    // INJECTING MENU ITEMS
 
-            document.querySelector('.dropdown-item.active')?.classList.remove('active');
-            document.querySelector('#' + page)?.classList.add('active');
-        }
-    })
-}
+    await customMenuItem('generate-image', 'Generate Image', 'javascript:void(0)', 17, 'far fa-image')
 
-const customMenuItem = (id, name, href, location, classes) => {
-    waitForSelector('[href="/my-account"]', (myAccountBtn) => {
-        var dropDownMenu = myAccountBtn.parentElement;
-        var menuItem = document.createElement("a")
-        menuItem.classList.add("dropdown-item");
-        menuItem.id = id;
-        menuItem.href = href;
-        menuItem.textContent = name;
-        if (classes) {
-            var iconEl = document.createElement("i");
-            iconEl.setAttribute("class", classes + " menu-icon")
+    // Credits
+    waitForSelector("footer .row", (footer) => {
+        var creditsRange = document.createRange();
+        var creditsHTML = creditsRange.createContextualFragment(`<div class="col-6 col-sm-4 col-lg py-1"><small>Using <a class="text-nowrap" href="https://chromewebstore.google.com/detail/cpphfojhgkppblcdhjbcploikanimgek" target="_blank">NameMC Extras</a></small></div>`);
+        footer?.insertBefore(creditsHTML, footer?.lastElementChild)
+    });
 
-            menuItem.insertBefore(iconEl, menuItem.lastChild);
-        }
-
-        dropDownMenu.insertBefore(menuItem, dropDownMenu.childNodes[location]);
-
-        var inject1 = document.createElement('script');
-        inject1.src = chrome.runtime.getURL(`dropdown-items/${id}.js`);
-        inject1.onload = function () {
-            this.remove();
-        };
-        (document.head || document.documentElement).appendChild(inject1);
-    })
-}
-
-// INJECTING PAGES
-
-customPage('skin-cape-test', 'Tester', 'Skin & Cape Tester', 'fas fa-rectangle-portrait')
-customPage('badges', 'Badges', 'Badges', 'fas fa-award')
-
-// INJECTING MENU ITEMS
-
-customMenuItem('generate-image', 'Generate Image', 'javascript:void(0)', 17, 'far fa-image')
-
-// Credits
-waitForSelector("footer .row", (footer) => {
-    var creditsRange = document.createRange();
-    var creditsHTML = creditsRange.createContextualFragment(`<div class="col-6 col-sm-4 col-lg py-1"><small>Using <a class="text-nowrap" href="https://chromewebstore.google.com/detail/cpphfojhgkppblcdhjbcploikanimgek" target="_blank">NameMC Extras</a></small></div>`);
-    footer?.insertBefore(creditsHTML, footer?.lastElementChild)
-});
+})()
