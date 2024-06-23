44ee8c9573f51c6daf5556a09ee91b83c59bd384
i4g <58300686+i4g@users.noreply.github.com>
2024-06-23 23:35:48 +0200

    fixing namemc's new uuid update

--- js/profile-page/profile-inject.js
+++ js/profile-page/profile-inject.js
@@ -296,7 +296,7 @@ if (location.pathname.split("-").length >= 5 || endsWithNumber(location.pathname
   waitForSelector('.order-lg-2', () => {
     var username = document.querySelector('.text-nowrap[translate=no]').innerText;
     var uuid = document.querySelector('.order-lg-2').innerText;
-    var views = document.querySelector('.card-body > :nth-child(3)');
+    var views = document.querySelector('.card-body > :nth-child(2)');
     var cardBody = document.querySelector('.card-body');
 
     // create layer button
