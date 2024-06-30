b205c05c039627ab51689fd16f6a5706a2157015
i4g <58300686+i4g@users.noreply.github.com>
2024-06-30 16:28:16 +0200

    changed cape to skin

--- js/skin-page/official-skin-page/official-skin-inject.js
+++ js/skin-page/official-skin-page/official-skin-inject.js
@@ -25,7 +25,7 @@ const createStealBtn = () => {
 
       // lazy to make the steal button make the heart btn move
       stealBtn.setAttribute('style', `width:36px;height:36px;margin-top:180px!important;`)
-      stealBtn.title = "Steal Cape";
+      stealBtn.title = "Steal Skin";
       stealIcon = document.createElement('i');
       stealIcon.classList.add('fas');
       stealIcon.classList.add('fa-user-secret');
