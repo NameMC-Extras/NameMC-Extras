6d862a1652ad9bc72c3728f151f89c6226dfc364
Faav <faavemc@gmail.com>
2025-05-17 14:12:59 -0400

    Preload every other

--- js/data-preload.js
+++ js/data-preload.js
@@ -1,7 +1,13 @@
 (async () => {
+  if (!localStorage['preloaded']) localStorage['preloaded'] = 0;
+  if (localStorage['preloaded'] === "1") {
+    localStorage['preloaded'] = 0;
+    return;
+  }
+  localStorage['preloaded'] = 1;
   async function fetchSupabase(endPoints) {
     return Promise.all([...endPoints.map(async endPoint => await fetch(`https://raw.githubusercontent.com/NameMC-Extras/data/main/${endPoint}.json`)),
-      await fetch('https://bedrock.lol/api/v1/capes')
+    await fetch('https://bedrock.lol/api/v1/capes')
     ]);
   };
 
@@ -20,7 +26,7 @@
     "user_capes": "user_capes",
     "user_emoji_overrides": "user_emoji_overrides",
   };
-  
+
   async function storeResults(results) {
     var datas = Promise.all(results.map(async (result, i) => {
       let keyName = Object.keys(endPoints)[i];
