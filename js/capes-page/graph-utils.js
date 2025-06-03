bc3c4b99244a06c7e38d3c9c683fca448a5017f0
Faav <faavemc@gmail.com>
2025-06-03 17:34:55 -0400

    v3.6.4

--- js/capes-page/graph-utils.js
+++ js/capes-page/graph-utils.js
@@ -63,7 +63,7 @@ class CapeUsageGraph {
     this.data = [];
     this.options = Object.assign({
       timeframe: 'week', // day, week, month, year, all
-      lineColor: (localStorage['customTheme'] === "true" && localStorage['customBtn']) || '#007bff',
+      lineColor: (localStorage['customTheme'] === "true" && localStorage['customBtn']) || '#236dad',
       gridColor: '#e9ecef',
       textColor: (localStorage['customTheme'] === "true" && localStorage['customText']) || getComputedStyle(document.documentElement).getPropertyValue("--bs-body-color"),
       padding: 40,
@@ -127,7 +127,7 @@ class CapeUsageGraph {
       tooltipContainer.className = 'graph-tooltip';
       tooltipContainer.style.position = 'absolute';
       tooltipContainer.style.backgroundColor = 'rgba(0,0,0,0.8)';
-      tooltipContainer.style.color = (localStorage['customTheme'] === "true" && localStorage['customText']) || getComputedStyle(document.documentElement).getPropertyValue("--bs-body-color");
+      tooltipContainer.style.color = (localStorage['customTheme'] === "true" && localStorage['customText']) || '#DEE2E6';
       tooltipContainer.style.padding = '5px 10px';
       tooltipContainer.style.borderRadius = '4px';
       tooltipContainer.style.fontSize = '12px';
